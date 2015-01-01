4e6589cea858cffe22c2ebbe72035b22666162c9
Evisceration <eviscerationls@gmail.com>
2015-01-01 14:29:50 +0000

    QuickSettings: Add Haptic Feedback to tiles [1/2]
    Change-Id: I792a00a2689a572110963db00362f8e9d77b316e
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9387,6 +9387,13 @@ public final class Settings {
         public static final String ALLOW_SIGNATURE_FAKE = "allow_signature_fake";
 
         /**
+         * Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
