cdc7119664df5e358c9dfe033d5c8c70fb054ab0
Carlo Savignano <carlosavignano@aospa.co>
2016-10-23 18:41:27 +0800

    pocket: introduce pocket judge
    * Judge if device is in pocket.
    * Notify clients callbacks when pocked state changes.
    * Start listening when device becomes not interactive.
    * Stop listening when device becomes interactive and is NOT in pocket.
    
    Ticket: NOUGAT-9.
    
    Change-Id: Ifcf59c172f55cce95a72411820723696ef2dd9ae
    Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: TheStrix <parthbhatia98@gmail.com>
    Signed-off-by: iKeramat <ikeramat80@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Ziauddin Sameer <ziauddinsameer@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5449,6 +5449,14 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
 
+        /**
+         * Whether allowing pocket service to register sensors and dispatch informations.
+         *   0 = disabled
+         *   1 = enabled
+         * @author Carlo Savignano
+         * @hide
+         */
+        public static final String POCKET_JUDGE = "pocket_judge";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
