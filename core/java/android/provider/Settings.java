321f96c640d0ae8f9ec7ccba74c840a15ebe6409
LorDClockaN <lordclockan@gmail.com>
2017-08-04 15:43:57 +0200

    base: Sensor block per-package switch (1/2)
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5700,6 +5700,12 @@ public final class Settings {
         public static final String VOLTE_VOWIFI_OVERRIDE = "volte_vowifi_override";
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
