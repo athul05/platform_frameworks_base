2d3042dc634a6ba3abe1ba84e69728f83abbd189
Alex Cruz <du.alexcruz@gmail.com>
2019-01-19 17:30:24 -0600

    ColtUtils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc
    Signed-off-by: Saikiran <bjsaikiran@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/com/android/internal/util/colt/ColtUtils.java
+++ core/java/com/android/internal/util/colt/ColtUtils.java
@@ -18,6 +18,7 @@ package com.android.internal.util.colt;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.content.om.IOverlayManager;
@@ -64,6 +65,19 @@ public class ColtUtils {
         if (pm!= null) {
             pm.goToSleep(SystemClock.uptimeMillis());
         }
+    }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
     }
 
     public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
