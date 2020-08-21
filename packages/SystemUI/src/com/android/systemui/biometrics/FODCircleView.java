c4666b315a9d50d0869345bac72a66cc101a8866
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-08-21 21:05:09 +0300

    FODCircleView: Enable hardware acceleration
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: Ic5425e7cfd54d7fc13519f80caf18b17ba49c4a3

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -338,7 +338,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener,Tu
         mParams.type = WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG;
         mParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
-                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
+                WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH |
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
         mParams.gravity = Gravity.TOP | Gravity.LEFT;
 
         mPressedParams.copyFrom(mParams);
