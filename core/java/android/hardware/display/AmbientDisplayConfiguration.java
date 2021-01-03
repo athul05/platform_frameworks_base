ezio84/abc-n_frameworks_base

Ambient music: pulse on new music tracks and Now Playing info [1/2]

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -53,7 +53,8 @@ public class AmbientDisplayConfiguration {
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
                 || doubleTapGestureEnabled(user)
-                || isPowerBtnFlashlightEnabled(user);
+                || isPowerBtnFlashlightEnabled(user)
+                || isAmbientTickerEnabled(user);
     }
 
     /** {@hide} */
@@ -224,5 +225,11 @@ public class AmbientDisplayConfiguration {
     public boolean isPowerBtnFlashlightEnabled(int user) {
         return Settings.Secure.getIntForUser(mContext.getContentResolver(),
                 Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0, user) != 0;
+    }
+
+    /** {@hide} */
+    public boolean isAmbientTickerEnabled(int user) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
     }
 }

athul05/platform_frameworks_base

Signed-off-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/android/hardware/display/AmbientDisplayConfiguration.java
@@ -52,7 +52,8 @@ public class AmbientDisplayConfiguration {
                 || wakeDisplayGestureEnabled(user)
                 || pickupGestureEnabled(user)
                 || tapGestureEnabled(user)
-                || doubleTapGestureEnabled(user);
+                || doubleTapGestureEnabled(user)
+                || isAmbientTickerEnabled(user);
     }
 
     /** {@hide} */
@@ -227,5 +228,11 @@ public class AmbientDisplayConfiguration {
     public boolean isAmbientGestureEnabled(int user) {
         return Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.AMBIENT_WAKE_GESTURES, 1, user) != 0;
+    }
+
+    /** {@hide} */
+    public boolean isAmbientTickerEnabled(int user) {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.PULSE_ON_NEW_TRACKS, 1, user) != 0;
     }
 }